2ff62aad76d6b7ed0d9c85d537c45d8e105ec1dd
Clémentbo <clementbo50@gmail.com>
2025-08-11 15:05:05 +0200

    Découverte des composants images & alertes attention ne alerte ne marche sur le web mais sur mobile

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import img from './assets/donut.jpg';
+import { Alert, Button, Image, StyleSheet, Text, View } from 'react-native';
 import { useContext, useEffect, useState } from 'react';
 import Board from './src/Board';
 import ThemeContextWrapper, { ThemeContext } from './src/themeContextWrapper';
+
 
 export default function App() {
 
   return (
     <ThemeContextWrapper>
-      <MyApp />
+       <MyApp />
+      {/*   <Image source ={img} /> */}
+      {/*  <Image style={{ width: 250, height: 250 }}source={{uri: "https://t3.ftcdn.net/jpg/02/45/78/98/360_F_245789812_U5YAKoU5bO1vPUlmMygG3RYwRQcDsjuD.jpg"}} /> */}
     </ThemeContextWrapper>
   );
 }
@@ -30,6 +34,8 @@ const MyApp = () => {
     <View style={styles.container}>
       <Board size={3} />
       <Button title="Changer de thème" onPress={() => toggleTheme() }></Button>
+      <Button title="Afficher une alerte" onPress={() => Alert.alert('Titre', 'Contenu de l\'alerte')} />
+
     </View>
   )
-}
+} 
